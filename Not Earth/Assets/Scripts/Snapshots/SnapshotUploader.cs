fd7135fd7eefa625ec8f75042a1efd927a3af2d3
Doug Guzman <swiftshadow34@gmail.com>
2019-11-21 21:49:38 -0600

    Adds a file uploader for the pictures

---
+++ Not Earth/Assets/Scripts/Snapshots/SnapshotUploader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGoogleDrive;
+
+public class SnapshotUploader : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Attempting upload");
+            byte[] bytes = new byte[10];
+            var file = new UnityGoogleDrive.Data.File { Name = "Screenshot.png", Content = bytes };
+            Debug.Log("Sending request");
+            GoogleDriveFiles.Create(file).Send();
+            Debug.Log("Request sent");
+        }
+    }
+}
